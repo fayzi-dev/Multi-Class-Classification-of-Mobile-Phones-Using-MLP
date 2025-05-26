f68c89cac29efda5286a791796a699dcc689b001
Mohammad Fayzi <fayzi.dev@gmail.com>
2025-05-26 04:39:13 +0330

    Dataloader & define model & Train loop and add class AverageMeter

--- Classification_MLP.py
+++ Classification_MLP.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
 import matplotlib.pyplot as plt
+from torch.testing._internal.common_quantization import AverageMeter
 
 df = pd.read_csv('train.csv')
 # print(df.info())
@@ -34,3 +35,81 @@ std = x_train.std(dim=0)
 x_train = (x_train - mu) / std
 x_valid = (x_valid - mu) / std
 
+# Dataloader
+from torch.utils.data import DataLoader, TensorDataset
+
+train_data = TensorDataset(x_train, y_train)
+train_loader = DataLoader(train_data, batch_size=64, shuffle=True)
+
+print(len(train_loader))  # 25
+x_batch, y_batch = next(iter(train_loader))
+print(x_batch.shape)  # torch.Size([64, 20])
+
+valid_data = TensorDataset(x_valid, y_valid)
+valid_loader = DataLoader(valid_data, batch_size=64, shuffle=True)
+
+# model
+num_features = 20
+num_classes = 4
+
+model = nn.Sequential(nn.Linear(num_features, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 32),
+                      nn.ReLU(),
+                      nn.Linear(32, num_classes))
+
+yp = model(x_batch)
+# print(yp[:2, :])
+xxx = torch.tensor([torch.numel(p) for p in model.parameters()])
+# print(xxx)
+
+# loss & optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+# Class AverageMeter
+from torch.testing._internal.common_quantization import AverageMeter
+
+
+class AverageMeter(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+# Train looooop
+num_epochs = 300
+
+for epoch in range(num_epochs):
+    loss_train = AverageMeter()
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        yp = model(x_batch)
+        loss = loss_fn(yp, y_batch)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        loss_train.update(loss.item())
+
+    with torch.no_grad():
+        loss_valid = AverageMeter()
+        for i, (x_batch, y_batch) in enumerate(valid_loader):
+            yp = model(x_batch)
+            loss = loss_fn(yp, y_batch)
+            loss_valid.update(loss.item())
+
+    print(f'Epoch{epoch}')
+    print(f'Train Loss:{loss_train.avg}')
+    print(f'Valid Loss:{loss_valid.avg}')
+    print()
